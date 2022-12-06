6303afa00a06549aaf28a5a8dba447309692f40f
aristokratos <stefaneey@gmail.com>
2022-12-06 15:28:13 +0100

    Logger

--- Repository/Repository/TransactionRepository.cs
+++ Repository/Repository/TransactionRepository.cs
@@ -1,4 +1,5 @@
 ﻿using E_Wallet_App.Domain.Models;
+using E_Wallet_App.Entity.Dtos;
 using E_WalletApp.CORE.Interface.RepoInterface;
 using E_WalletApp.DB.Context;
 using Microsoft.EntityFrameworkCore;
@@ -27,4 +28,11 @@ namespace E_WalletRepository.Repository
         //    return user;
         //}
     }
+    //public async Task<TransDto> GetWalletDetails(string Currency, Guid walletAddress)
+    //{
+    //    var walletDetails = new _context.Wallets.FirstOrDefaultAsync(x => x.WalletAdress,
+    //        WalletBalance = $"{Currency.ToUpper()} {Math.Round(walletAddress.WalletBalance * await getApi(Currency), 3)}");
+    //}
+    //return walletUserView;
+       
 }
